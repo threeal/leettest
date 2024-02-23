03eca83ea47659217df8f777559f2e5843d0a90f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-23 19:54:59 +0700

    feat: add compile step to the main entry point

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
+import { globSync } from "glob";
+import { execSync } from "node:child_process";
+import { mkdirSync } from "node:fs";
+import path from "node:path";
 import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
@@ -11,7 +15,18 @@ yargs(hideBin(process.argv))
     "Compile and test solutions to LeetCode's problems",
     (yargs) => yargs,
     () => {
-      // TODO
+      const testFiles = globSync("**/test.cpp");
+      for (const testFile of testFiles) {
+        process.stdout.write(`Compiling ${testFile}...\n`);
+
+        const buildDir = path.join("build", path.dirname(testFile));
+        mkdirSync(buildDir, { recursive: true });
+
+        const testExec = path.join(buildDir, "test");
+        execSync(`clang++ --std=c++20 ${testFile} -o ${testExec}`, {
+          stdio: "inherit",
+        });
+      }
     },
   )
   .parse();
