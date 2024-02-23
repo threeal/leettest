cf6d50d34e45ddf330d89ba2bdd321399d6b08cf
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-23 20:12:30 +0700

    feat: modify main entry point to compile and test solutions (#7)
    * feat: modify main entry point to compile and test solutions
    
    * feat: add compile step to the main entry point
    
    * feat: add run test step to the main entry point
    
    * ci(test): modify `test-sample` job to run main entry point script
    
    * chore: remove unused `sample/test.sh` script

--- src/bin.ts
+++ src/bin.ts
@@ -1,23 +1,35 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
+import { globSync } from "glob";
+import { execSync } from "node:child_process";
+import { mkdirSync } from "node:fs";
+import path from "node:path";
 import { hideBin } from "yargs/helpers";
-import { fibonacciSequence } from "./sequence.js";
 
 yargs(hideBin(process.argv))
-  .scriptName("my_fibonacci")
-  .version("0.0.0")
+  .scriptName("leetsolve")
+  .version("0.1.0")
   .command(
-    "$0 <n>",
-    "Generate a Fibonacci sequence up to the given number of terms.",
-    (yargs) =>
-      yargs.positional("n", {
-        demandOption: true,
-        describe: "The number of terms",
-        type: "number",
-      }),
-    (argv) => {
-      process.stdout.write(fibonacciSequence(argv.n).join(" ") + "\n");
+    "$0",
+    "Compile and test solutions to LeetCode's problems",
+    (yargs) => yargs,
+    () => {
+      const testFiles = globSync("**/test.cpp");
+      for (const testFile of testFiles) {
+        process.stdout.write(`Compiling ${testFile}...\n`);
+
+        const buildDir = path.join("build", path.dirname(testFile));
+        mkdirSync(buildDir, { recursive: true });
+
+        const testExec = path.join(buildDir, "test");
+        execSync(`clang++ --std=c++20 ${testFile} -o ${testExec}`, {
+          stdio: "inherit",
+        });
+
+        process.stdout.write(`Running ${testExec}...\n`);
+        execSync(testExec, { stdio: "inherit" });
+      }
     },
   )
   .parse();
