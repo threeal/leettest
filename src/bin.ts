d7f70a1f3dcb8074c050354f7e69559886994f7b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-23 19:26:51 +0700

    feat: modify main entry point to compile and test solutions

--- src/bin.ts
+++ src/bin.ts
@@ -2,22 +2,16 @@
 
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import { fibonacciSequence } from "./sequence.js";
 
 yargs(hideBin(process.argv))
-  .scriptName("my_fibonacci")
-  .version("0.0.0")
+  .scriptName("leetsolve")
+  .version("0.1.0")
   .command(
-    "$0 <n>",
-    "Generate a Fibonacci sequence up to the given number of terms.",
-    (yargs) =>
-      yargs.positional("n", {
-        demandOption: true,
-        describe: "The number of terms",
-        type: "number",
-      }),
-    (argv) => {
-      process.stdout.write(fibonacciSequence(argv.n).join(" ") + "\n");
+    "$0",
+    "Compile and test solutions to LeetCode's problems",
+    (yargs) => yargs,
+    () => {
+      // TODO
     },
   )
   .parse();
