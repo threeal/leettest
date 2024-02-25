52918c3183d85470a815b120477b44f5d4410d7c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-25 22:10:02 +0700

    test: add unit testing to C++ test functions (#13)
    * test: integrate `jest-extended` in testing
    
    * test: set `NODE_OPTIONS` in the `test` script
    
    * test: add testing for `compileCppTest` function
    
    * test: add testing for `runCppTest` function

---
+++ src/test/cpp.test.ts
@@ -0,0 +1,50 @@
+import { jest } from "@jest/globals";
+import "jest-extended";
+
+jest.unstable_mockModule("node:child_process", () => ({
+  execSync: jest.fn(),
+}));
+
+jest.unstable_mockModule("node:fs", () => ({
+  mkdirSync: jest.fn(),
+}));
+
+beforeEach(async () => {
+  const { execSync } = await import("node:child_process");
+  const { mkdirSync } = await import("node:fs");
+
+  jest.mocked(execSync).mockClear();
+  jest.mocked(mkdirSync).mockClear();
+});
+
+it("should compile a C++ test file", async () => {
+  const { execSync } = await import("node:child_process");
+  const { mkdirSync } = await import("node:fs");
+  const { compileCppTest } = await import("./cpp.js");
+
+  const testExec = compileCppTest("path/to/test.cpp");
+
+  expect(mkdirSync).toHaveBeenCalledExactlyOnceWith("build/path/to", {
+    recursive: true,
+  });
+  expect(execSync).toHaveBeenCalledExactlyOnceWith(
+    "clang++ --std=c++20 path/to/test.cpp -o build/path/to/test",
+    {
+      stdio: "inherit",
+    },
+  );
+  expect(execSync).toHaveBeenCalledAfter(jest.mocked(mkdirSync));
+
+  expect(testExec).toBe("build/path/to/test");
+});
+
+it("should run a C++ test executable", async () => {
+  const { execSync } = await import("node:child_process");
+  const { runCppTest } = await import("./cpp.js");
+
+  runCppTest("build/path/to/test");
+
+  expect(execSync).toHaveBeenCalledExactlyOnceWith("build/path/to/test", {
+    stdio: "inherit",
+  });
+});
