ccc0011a0ac68f7f803007cd364413021d96dd05
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-26 17:14:12 +0700

    chore: remove unused `src/sequence.ts` module (#15)

--- src/sequence.test.ts
+++
@@ -1,7 +0,0 @@
-import { fibonacciSequence } from "./sequence.js";
-
-it("should generate a fibonacci sequence", () => {
-  expect(fibonacciSequence(1)).toStrictEqual([1]);
-  expect(fibonacciSequence(2)).toStrictEqual([1, 1]);
-  expect(fibonacciSequence(5)).toStrictEqual([1, 1, 2, 3, 5]);
-});
