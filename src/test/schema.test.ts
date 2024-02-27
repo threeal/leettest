6b0fb29f4c92b988409b6ea64162aae9c8c5b5d0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-27 18:30:12 +0700

    test: add test for `readYamlSchema` function

---
+++ src/test/schema.test.ts
@@ -0,0 +1,64 @@
+import { jest } from "@jest/globals";
+import "jest-extended";
+
+jest.unstable_mockModule("node:fs", () => ({
+  readFileSync: jest.fn(),
+}));
+
+it("should read a YAML schema file", async () => {
+  const { readFileSync } = await import("node:fs");
+  const { readYamlSchema } = await import("./schema.js");
+
+  jest.mocked(readFileSync).mockReturnValue(`
+cpp:
+  function:
+    name: sum
+    inputs: [int, int]
+    output: int
+
+examples:
+  1:
+    inputs:
+      num1: 12
+      num2: 5
+    output: 17
+
+  2:
+    inputs:
+      num1: -10
+      num2: 4
+    output: -6
+  `);
+
+  const schema = readYamlSchema("path/to/test.yaml");
+
+  expect(readFileSync).toHaveBeenCalledExactlyOnceWith(
+    "path/to/test.yaml",
+    "utf-8",
+  );
+  expect(schema).toStrictEqual({
+    cpp: {
+      function: {
+        name: "sum",
+        inputs: ["int", "int"],
+        output: "int",
+      },
+    },
+    examples: {
+      1: {
+        inputs: {
+          num1: 12,
+          num2: 5,
+        },
+        output: 17,
+      },
+      2: {
+        inputs: {
+          num1: -10,
+          num2: 4,
+        },
+        output: -6,
+      },
+    },
+  });
+});
