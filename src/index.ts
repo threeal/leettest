b635c6a1176d1b12d3c23103ca92bd8fdbeb9569
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-27 18:32:55 +0700

    feat: add function for reading YAML schemas (#33)
    * test: add sample YAML schema for problem 2235
    
    * feat: add `readYamlSchema` function
    
    * test: add test for `readYamlSchema` function
    
    * feat: add `Schema` interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { testCppSolution } from "./test/cpp.js";
+export { readYamlSchema, Schema } from "./test/schema.js";
---
+++ src/test/schema.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from "node:fs";
+import YAML from "yaml";
+
+export interface Schema {
+  cpp: {
+    function: {
+      name: string;
+      inputs: string[];
+      output: string[];
+    };
+  };
+  examples: {
+    [key: string]: {
+      inputs: { [key: string]: unknown };
+      output: unknown;
+    };
+  };
+}
+
+/**
+ * Reads a test schema from a YAML file.
+ *
+ * @param schemaFile - The path of the YAML schema file.
+ * @returns The parsed test schema.
+ */
+export function readYamlSchema(schemaFile: string): Schema {
+  const data = readFileSync(schemaFile, "utf-8");
+  return YAML.parse(data);
+}
