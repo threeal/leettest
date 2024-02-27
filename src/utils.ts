1f261231a5b8e1dcd8a4c10975b6ca34a947c286
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-27 13:43:35 +0700

    DONT MERGE

---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+/**
+ * Retrieves the build directory of the given file path.
+ *
+ * This function will return the build directory of the given file path.
+ * If the file path is in the build directory, it will return the file path directory.
+ *
+ * @param filePath - The path of the file.
+ * @returns The path of the build directory.
+ */
+export function getBuildDirOfFile(filePath: string): string {
+
+}
