3cd4f9bc3a1b21c2947d9c37b4562904d724ab8f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-04 20:36:08 +0700

    fix: fix command for running C++ test on Windows

--- src/test/cpp/run.test.ts
+++ src/test/cpp/run.test.ts
@@ -9,9 +9,25 @@ it("should run a C++ test executable", async () => {
   const { execSync } = await import("node:child_process");
   const { runCppTest } = await import("./run.js");
 
+  jest.mocked(execSync).mockClear();
+
   runCppTest("build/path/to/test");
 
   expect(execSync).toHaveBeenCalledExactlyOnceWith("build/path/to/test", {
+    stdio: "pipe",
+  });
+});
+
+it("should run a C++ test executable on Windows", async () => {
+  const { execSync } = await import("node:child_process");
+  const { runCppTest } = await import("./run.js");
+
+  jest.mocked(execSync).mockClear();
+  Object.defineProperty(process, "platform", { value: "win32" });
+
+  runCppTest("build/path/to/test");
+
+  expect(execSync).toHaveBeenCalledExactlyOnceWith("start build/path/to/test", {
     stdio: "pipe",
   });
 });
