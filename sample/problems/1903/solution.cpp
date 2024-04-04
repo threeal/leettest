12e802d68a58adc8dc341669c471b66d6dd98017
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-04 21:30:30 +0700

    test: add problem 1903 to the sample problems

---
+++ sample/problems/1903/solution.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class Solution {
+ public:
+  std::string largestOddNumber(std::string num) {
+    for (std::size_t i = num.size(); i > 0; --i) {
+      if ((num[i - 1] - '0') % 2 != 1) continue;
+      return num.substr(0, i);
+    }
+
+    return "";
+  }
+};
