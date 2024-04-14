e03c1b96b35fb13b042323477a75fe2c53a04342
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-14 20:23:59 +0700

    refactor: add `testSolutions` function refactored from lines in `bin.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { testCppSolution } from "./test/cpp/index.js";
+export { testSolutions } from "./test/index.js";
 export { readYamlSchema, Schema } from "./test/schema.js";
 export { searchSolutions } from "./search.js";
---
+++ src/test/index.ts
@@ -0,0 +1,32 @@
+import { getErrorMessage } from "catched-error-message";
+import ora from "ora";
+import { testCppSolution } from "./cpp/index.js";
+
+/**
+ * Tests solutions to LeetCode problems.
+ *
+ * @param solutionFiles - An array containing the paths of the solution files to process.
+ * @returns A promise that resolves to the number of failed solution files to test.
+ */
+export async function testSolutions(solutionFiles: string[]): Promise<number> {
+  const tests = solutionFiles.map((file) => ({
+    file: file,
+    prom: testCppSolution(file),
+  }));
+
+  let failures = 0;
+  const spinner = ora();
+  for (const test of tests) {
+    spinner.start(`Testing ${test.file}...`);
+    try {
+      await test.prom;
+      spinner.succeed(`Tested ${test.file}`);
+    } catch (err) {
+      ++failures;
+      const msg = getErrorMessage(err).replaceAll("\n", "\n  ").trimEnd();
+      spinner.fail(`Failed to test ${test.file}\n  ${msg}\n`);
+    }
+  }
+
+  return failures;
+}
