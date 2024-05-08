86f10ec82f7be703571bff4dd224cb3f2d82bdc5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-09 00:09:43 +0700

    test: add edge case test when compiling C++

--- src/test/cpp/compile.test.ts
+++ src/test/cpp/compile.test.ts
@@ -31,3 +31,21 @@ it("should compile a C++ test file", async () => {
 
   await fs.access(executablePath, fs.constants.X_OK);
 });
+
+it("should not compile an invalid C++ test file", async () => {
+  const sourcePath = path.join(testDir.path, "test.cpp");
+  await fs.writeFile(sourcePath, "int main() {");
+
+  const executablePath = path.join(testDir.path, "build", "test");
+  await expect(compileCppTest(sourcePath, executablePath)).rejects.toThrow(
+    /Command failed:[^]*1 error generated/,
+  );
+});
+
+it("should not compile a non-existing C++ test file", async () => {
+  const sourcePath = path.join(testDir.path, "test.cpp");
+  const executablePath = path.join(testDir.path, "build", "test");
+  await expect(compileCppTest(sourcePath, executablePath)).rejects.toThrow(
+    /Command failed:[^]*no such file or directory/,
+  );
+});
