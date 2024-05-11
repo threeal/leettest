161cdf01578aa8d1a79e78c77004ea182365746d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-11 22:05:07 +0700

    feat: modify C++ compilation output to have `.exe` extension on Windows (#228)
    * feat: add `getCppExecutablePath`
    
    * test: group compile C++ testing

--- src/test/cpp/compile.test.ts
+++ src/test/cpp/compile.test.ts
@@ -1,65 +1,89 @@
 import { createTempDirectory, ITempDirectory } from "create-temp-directory";
 import fs from "node:fs/promises";
 import path from "node:path";
-import { compileCppTest } from "./compile.js";
+import { compileCppTest, getCppExecutablePath } from "./compile.js";
 
-let testDir: ITempDirectory;
+describe("retrieve an executable path", () => {
+  let originalProcessPlatform: string;
+  beforeEach(() => (originalProcessPlatform = process.platform));
+  afterEach(() =>
+    Object.defineProperty(process, "platform", {
+      value: originalProcessPlatform,
+    }),
+  );
 
-beforeEach(async () => {
-  testDir = await createTempDirectory();
-});
-
-afterEach(async () => {
-  await testDir.remove();
-});
-
-describe("compile a C++ test file", () => {
-  let sourcePath: string;
-  beforeEach(async () => {
-    sourcePath = path.join(testDir.path, "test.cpp");
-    await fs.writeFile(
-      sourcePath,
-      [
-        `#include <iostream>`,
-        ``,
-        `int main() {`,
-        `  std::cout << "Hello world!\\n";`,
-        `  return 0;`,
-        `}`,
-      ].join("\n"),
+  it("should retrieve an executable path on Windows platform", () => {
+    Object.defineProperty(process, "platform", { value: "win32" });
+    expect(getCppExecutablePath(path.join("path", "to", "test.cpp"))).toBe(
+      path.join("path", "to", "test.exe"),
     );
   });
 
-  it("should compile a C++ test file", async () => {
-    const executablePath = await compileCppTest(sourcePath);
-
-    expect(executablePath).toBe(path.join(testDir.path, "test"));
-    await fs.access(executablePath, fs.constants.X_OK);
-  }, 60000);
-
-  it("should compile a C++ test file to a specified directory", async () => {
-    const executablePath = await compileCppTest(
-      sourcePath,
-      path.join(testDir.path, "build"),
+  it("should retrieve an executable path on non-Windows platform", () => {
+    Object.defineProperty(process, "platform", { value: "non-win32" });
+    expect(getCppExecutablePath(path.join("path", "to", "test.cpp"))).toBe(
+      path.join("path", "to", "test"),
     );
-
-    expect(executablePath).toBe(path.join(testDir.path, "build", "test"));
-    await fs.access(executablePath, fs.constants.X_OK);
-  }, 60000);
+  });
 });
 
-it("should not compile an invalid C++ test file", async () => {
-  const sourcePath = path.join(testDir.path, "test.cpp");
-  await fs.writeFile(sourcePath, "int main() {");
+describe("compile a C++ test file", () => {
+  let testDir: ITempDirectory;
+  beforeEach(async () => (testDir = await createTempDirectory()));
+  afterEach(async () => await testDir.remove());
 
-  await expect(compileCppTest(sourcePath)).rejects.toThrow(
-    /Command failed:[^]*1 error generated/,
-  );
-}, 60000);
+  describe("compile a valid C++ test file", () => {
+    let sourcePath: string;
+    beforeEach(async () => {
+      sourcePath = path.join(testDir.path, "test.cpp");
+      await fs.writeFile(
+        sourcePath,
+        [
+          `#include <iostream>`,
+          ``,
+          `int main() {`,
+          `  std::cout << "Hello world!\\n";`,
+          `  return 0;`,
+          `}`,
+        ].join("\n"),
+      );
+    });
 
-it("should not compile a non-existing C++ test file", async () => {
-  const sourcePath = path.join(testDir.path, "test.cpp");
-  await expect(compileCppTest(sourcePath)).rejects.toThrow(
-    /Command failed:[^]*no such file or directory/,
-  );
-}, 60000);
+    it("should compile a valid C++ test file", async () => {
+      const executablePath = await compileCppTest(sourcePath);
+
+      expect(executablePath).toBe(
+        getCppExecutablePath(path.join(testDir.path, "test")),
+      );
+      await fs.access(executablePath, fs.constants.X_OK);
+    }, 60000);
+
+    it("should compile a valid C++ test file to a specified directory", async () => {
+      const executablePath = await compileCppTest(
+        sourcePath,
+        path.join(testDir.path, "build"),
+      );
+
+      expect(executablePath).toBe(
+        getCppExecutablePath(path.join(testDir.path, "build", "test")),
+      );
+      await fs.access(executablePath, fs.constants.X_OK);
+    }, 60000);
+  });
+
+  it("should not compile an invalid C++ test file", async () => {
+    const sourcePath = path.join(testDir.path, "test.cpp");
+    await fs.writeFile(sourcePath, "int main() {");
+
+    await expect(compileCppTest(sourcePath)).rejects.toThrow(
+      /Command failed:[^]*1 error generated/,
+    );
+  }, 60000);
+
+  it("should not compile a non-existing C++ test file", async () => {
+    const sourcePath = path.join(testDir.path, "test.cpp");
+    await expect(compileCppTest(sourcePath)).rejects.toThrow(
+      /Command failed:[^]*no such file or directory/,
+    );
+  }, 60000);
+});
