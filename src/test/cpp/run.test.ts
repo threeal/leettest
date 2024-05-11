f846ce59cbc8183c6aeb5396852a5b1ffa2f411a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-11 22:46:40 +0700

    perf: utilize `execFile` in `runCppTest` function

--- src/test/cpp/run.test.ts
+++ src/test/cpp/run.test.ts
@@ -2,16 +2,16 @@ import { jest } from "@jest/globals";
 import "jest-extended";
 
 jest.unstable_mockModule("node:child_process", () => ({
-  exec: jest.fn((_, callback: () => void) => callback()),
+  execFile: jest.fn((_, callback: () => void) => callback()),
 }));
 
 it("should run a C++ test executable", async () => {
-  const { exec } = await import("node:child_process");
+  const { execFile } = await import("node:child_process");
   const { runCppTest } = await import("./run.js");
 
-  jest.mocked(exec).mockClear();
+  jest.mocked(execFile).mockClear();
 
   await expect(runCppTest("build/path/to/test")).resolves.toBeUndefined();
 
-  expect(jest.mocked(exec).mock.calls[0][0]).toBe("build/path/to/test");
+  expect(jest.mocked(execFile).mock.calls[0][0]).toBe("build/path/to/test");
 });
