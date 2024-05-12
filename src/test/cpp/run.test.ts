7418b6d2a76d7eb4b2cd2def95584c6a8fafa2c5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-12 23:34:40 +0700

    test: add edge case test when running C++ executable

--- src/test/cpp/run.test.ts
+++ src/test/cpp/run.test.ts
@@ -1,7 +1,7 @@
 import { createTempDirectory, ITempDirectory } from "create-temp-directory";
 import fs from "node:fs/promises";
 import path from "node:path";
-import { compileCppTest } from "./compile.js";
+import { compileCppTest, getCppExecutablePath } from "./compile.js";
 import { runCppTest } from "./run.js";
 
 const testDirs: ITempDirectory[] = [];
@@ -22,6 +22,45 @@ it.concurrent(
     const executablePath = await compileCppTest(sourcePath);
 
     await runCppTest(executablePath);
+  },
+  60000,
+);
+
+it.concurrent(
+  "should run a failing C++ test executable",
+  async () => {
+    const testDir = await getTestDir();
+
+    const sourcePath = path.join(testDir.path, "test.cpp");
+    await fs.writeFile(
+      sourcePath,
+      [
+        `#include <iostream>`,
+        ``,
+        `int main() {`,
+        `  std::cerr << "unknown error\\n";`,
+        `  return 1;`,
+        `}`,
+      ].join("\n"),
+    );
+
+    const executablePath = await compileCppTest(sourcePath);
+
+    await expect(runCppTest(executablePath)).rejects.toThrow(
+      /Command failed:[^]*unknown error/,
+    );
+  },
+  60000,
+);
+
+it.concurrent(
+  "should not run a non-existing C++ test executable",
+  async () => {
+    const testDir = await getTestDir();
+    const executablePath = getCppExecutablePath(
+      path.join(testDir.path, "test"),
+    );
+    await expect(runCppTest(executablePath)).rejects.toThrow(/spawn.*ENOENT/);
   },
   60000,
 );
