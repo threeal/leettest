01e691b9b541d5149f9156df86fc7dd359ea3839
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-21 23:03:40 +0700

    DONT MERGE

--- src/test/cpp/generate/format.test.ts
+++ src/test/cpp/generate/format.test.ts
@@ -1,17 +1,59 @@
+import { createTempDirectory, ITempDirectory } from "create-temp-directory";
+import fs from "node:fs/promises";
+import path from "node:path";
+import { compileCppTest } from "../compile.js";
+import { runCppTest } from "../run.js";
 import { formatCpp } from "./format.js";
 
+const testDirs: ITempDirectory[] = [];
+const compileAndRun = async (lines: string[]) => {
+  const testDir = await createTempDirectory();
+  testDirs.push(testDir);
+
+  const sourcePath = path.join(testDir.path, "main.cpp");
+  await fs.writeFile(sourcePath, lines.join("\n"));
+
+  const executablePath = await compileCppTest(sourcePath);
+  await runCppTest(executablePath);
+}
+
 describe("format values in C++ format", () => {
-  it("should format an integer", () => {
-    expect(formatCpp(123, "int")).toBe("123");
-  });
+  it.concurrent("should format an integer in C++ format", async () => {
+    await compileAndRun([
+      `#include <cassert>`,
+      ``,
+      `int main() {`,
+      `  int val = ${formatCpp(123, "int")};`,
+      `  assert(val == 123);`,
+      `};`,
+      ``
+    ]);
+  }, 60000);
 
-  it("should format a character", () => {
-    expect(formatCpp("c", "char")).toBe(`'c'`);
-  });
+  it.concurrent("should format a character in C++ format", async () => {
+    await compileAndRun([
+      `#include <cassert>`,
+      ``,
+      `int main() {`,
+      `  char val = ${formatCpp("c", "char")};`,
+      `  assert(val == 'c');`,
+      `};`,
+      ``
+    ]);
+  }, 60000);
 
-  it("should format a string", () => {
-    expect(formatCpp("something", "std::string")).toBe(`"something"`);
-  });
+  it.concurrent("should format a string in C++ format", async () => {
+    await compileAndRun([
+      `#include <cassert>`,
+      `#include <string>`,
+      ``,
+      `int main() {`,
+      `  std::string val = ${formatCpp("something", "std::string")};`,
+      `  assert(val == "something");`,
+      `};`,
+      ``
+    ]);
+  }, 60000);
 
   it("should format an array of integers", () => {
     expect(formatCpp([123, 234, 345], "std::vector<int>")).toBe(
@@ -24,4 +66,8 @@ describe("format values in C++ format", () => {
       `{"foo", "bar", "baz"}`,
     );
   });
+});
+
+afterAll(async () => {
+  await Promise.all(testDirs.map((testDir) => testDir.remove()));
 });
