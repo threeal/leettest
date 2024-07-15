60ea51453ee14a30d83c2c17f49bf66a66cd2b8b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-15 18:57:57 +0700

    test: update test to ensure finding correct GCC executable

--- src/compile/c.test.ts
+++ src/compile/c.test.ts
@@ -1,7 +1,14 @@
+import { execFile } from "node:child_process";
 import fs from "node:fs/promises";
+import { promisify } from "node:util";
 import { findGccExecutable } from "./c.js";
+
+const execFilePromise = promisify(execFile);
 
 it.concurrent("should find the GCC executable", async () => {
   const exeFile = await findGccExecutable();
   await fs.access(exeFile, fs.constants.X_OK);
+
+  const { stdout } = await execFilePromise(exeFile, ["--version"]);
+  expect(stdout).toMatch("gcc");
 });
