93e0b696e965ae4d51eb2ba9c543b2e511fe0f0a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-15 16:49:56 +0700

    test: modify `getExecutableFromSource` to be run sequentially

--- src/compile/utils.test.ts
+++ src/compile/utils.test.ts
@@ -43,36 +43,28 @@ describe("find an executable file", () => {
 
 describe("retrieve an executable file path", () => {
   let originalPlatform: string | undefined = undefined;
-
   const mockPlatform = (newPlatform: string) => {
     if (originalPlatform === undefined) originalPlatform = process.platform;
     Object.defineProperty(process, "platform", { value: newPlatform });
   };
 
-  const restorePlatform = () => {
-    if (originalPlatform === undefined) return;
-    Object.defineProperty(process, "platform", { value: originalPlatform });
-  };
+  it("should retrieve an executable file path on Windows platform", () => {
+    mockPlatform("win32");
+    expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
+      path.join("path", "to", "main.exe"),
+    );
+  });
 
-  it.concurrent(
-    "should retrieve an executable file path on Windows platform",
-    () => {
-      mockPlatform("win32");
-      expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
-        path.join("path", "to", "main.exe"),
-      );
-    },
-  );
+  it("should retrieve an executable file path on non-Windows platform", () => {
+    mockPlatform("non-win32");
+    expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
+      path.join("path", "to", "main"),
+    );
+  });
 
-  it.concurrent(
-    "should retrieve an executable file path on non-Windows platform",
-    () => {
-      mockPlatform("non-win32");
-      expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
-        path.join("path", "to", "main"),
-      );
-    },
-  );
-
-  afterAll(() => restorePlatform());
+  afterEach(() => {
+    if (originalPlatform !== undefined) {
+      Object.defineProperty(process, "platform", { value: originalPlatform });
+    }
+  });
 });
