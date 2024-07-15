41bffad40b51717b961e36fa844426d577865408
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-15 15:12:27 +0700

    test: organize `getExecutableFromSource` test in one block

--- src/compile/utils.test.ts
+++ src/compile/utils.test.ts
@@ -1,36 +1,38 @@
 import path from "node:path";
 import { getExecutableFromSource } from "./utils.js";
 
-let originalPlatform: string | undefined = undefined;
+describe("retrieve an executable file path", () => {
+  let originalPlatform: string | undefined = undefined;
 
-const mockPlatform = (newPlatform: string) => {
-  if (originalPlatform === undefined) originalPlatform = process.platform;
-  Object.defineProperty(process, "platform", { value: newPlatform });
-};
+  const mockPlatform = (newPlatform: string) => {
+    if (originalPlatform === undefined) originalPlatform = process.platform;
+    Object.defineProperty(process, "platform", { value: newPlatform });
+  };
 
-const restorePlatform = () => {
-  if (originalPlatform === undefined) return;
-  Object.defineProperty(process, "platform", { value: originalPlatform });
-};
+  const restorePlatform = () => {
+    if (originalPlatform === undefined) return;
+    Object.defineProperty(process, "platform", { value: originalPlatform });
+  };
 
-it.concurrent(
-  "should retrieve an executable file path on Windows platform",
-  () => {
-    mockPlatform("win32");
-    expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
-      path.join("path", "to", "main.exe"),
-    );
-  },
-);
+  it.concurrent(
+    "should retrieve an executable file path on Windows platform",
+    () => {
+      mockPlatform("win32");
+      expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
+        path.join("path", "to", "main.exe"),
+      );
+    },
+  );
 
-it.concurrent(
-  "should retrieve an executable file path on non-Windows platform",
-  () => {
-    mockPlatform("non-win32");
-    expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
-      path.join("path", "to", "main"),
-    );
-  },
-);
+  it.concurrent(
+    "should retrieve an executable file path on non-Windows platform",
+    () => {
+      mockPlatform("non-win32");
+      expect(getExecutableFromSource(path.join("path", "to", "main.cpp"))).toBe(
+        path.join("path", "to", "main"),
+      );
+    },
+  );
 
-afterAll(() => restorePlatform());
+  afterAll(() => restorePlatform());
+});
