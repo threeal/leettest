b0fd704957fe044e9020b2678619dc6ae9c59efd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-15 20:42:22 +0700

    feat: add `compileCSource` function

--- src/compile/c.ts
+++ src/compile/c.ts
@@ -1,4 +1,10 @@
-import { findExecutable } from "./utils.js";
+import { execFile } from "node:child_process";
+import { mkdir } from "node:fs/promises";
+import { promisify } from "node:util";
+import path from "node:path";
+import { findExecutable, getExecutableFromSource } from "./utils.js";
+
+const execFilePromise = promisify(execFile);
 
 /**
  * Finds the GCC executable file.
@@ -7,4 +13,25 @@ import { findExecutable } from "./utils.js";
  */
 export async function findGccExecutable(): Promise<string> {
   return await findExecutable("gcc-13", "gcc");
+}
+
+/**
+ * Compiles a C source file using GCC.
+ *
+ * @param sourceFile - The path of the C source file to compile.
+ * @param outDir - An optional output directory for the compiled executable file.
+ * @returns A promise that resolves to the path of the compiled executable file.
+ */
+export async function compileCSource(
+  sourceFile: string,
+  outDir?: string,
+): Promise<string> {
+  const gccExeFile = await findGccExecutable();
+  let exeFile = getExecutableFromSource(sourceFile);
+  if (outDir !== undefined) {
+    await mkdir(outDir, { recursive: true });
+    exeFile = path.join(outDir, path.basename(exeFile));
+  }
+  await execFilePromise(gccExeFile, ["-O2", sourceFile, "-o", exeFile]);
+  return exeFile;
 }
