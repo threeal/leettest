edf1e87af17eae31b44dd20150c721c68ecaf37b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-18 22:29:12 +0700

    test: modify some sample problems to receive inputs as references

--- sample/problems/appendIntegersArrays/solution.cpp
+++ sample/problems/appendIntegersArrays/solution.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
  public:
-  std::vector<int> appendIntegersArrays(std::vector<int> nums1, std::vector<int> nums2) {
+  std::vector<int> appendIntegersArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
     nums1.insert(nums1.end(), nums2.begin(), nums2.end());
     return nums1;
   }
