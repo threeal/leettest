f3c44bc0a9b83e1a8ccd3987d757556cc864a73f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-18 20:51:49 +0700

    feat: add a new `parseCppTestSchema` function

---
+++ src/test/schema/cpp.test.ts
@@ -0,0 +1,82 @@
+import { parseCppTestSchema } from "./cpp.js";
+
+it("should parse a C++ test schema from a raw test schema", () => {
+  const schema = parseCppTestSchema({
+    cpp: {
+      function: {
+        name: "sum",
+        arguments: ["num1", "num2"],
+      },
+      inputs: {
+        num1: "int",
+        num2: "int",
+      },
+      output: "int",
+    },
+    cases: [
+      {
+        name: "example 1",
+        inputs: {
+          num1: 12,
+          num2: 5,
+        },
+        output: 17,
+      },
+      {
+        name: "example 2",
+        inputs: {
+          num1: -10,
+          num2: 4,
+        },
+        output: -6,
+      },
+    ],
+  });
+
+  expect(schema).toEqual({
+    cases: [
+      {
+        name: "example 1",
+        function: {
+          name: "sum",
+          arguments: ["num1", "num2"],
+        },
+        inputs: {
+          num1: {
+            type: "int",
+            value: 12,
+          },
+          num2: {
+            type: "int",
+            value: 5,
+          },
+        },
+        output: {
+          type: "int",
+          value: 17,
+        },
+      },
+      {
+        name: "example 2",
+        function: {
+          name: "sum",
+          arguments: ["num1", "num2"],
+        },
+        inputs: {
+          num1: {
+            type: "int",
+            value: -10,
+          },
+          num2: {
+            type: "int",
+            value: 4,
+          },
+        },
+        output: {
+          type: "int",
+          value: -6,
+        },
+      },
+    ],
+  });
+});
