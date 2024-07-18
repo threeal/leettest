8f170deae1f87e26d56e2409210c9093163811ba
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-18 22:33:34 +0700

    test: modify some sample problems to receive inputs as references (#342)

--- sample/problems/appendStringsArrays/solution.cpp
+++ sample/problems/appendStringsArrays/solution.cpp
@@ -3,7 +3,7 @@
 
 class Solution {
  public:
-  std::vector<std::string> appendStringsArrays(std::vector<std::string> strs1, std::vector<std::string> strs2) {
+  std::vector<std::string> appendStringsArrays(std::vector<std::string>& strs1, std::vector<std::string>& strs2) {
     strs1.insert(strs1.end(), strs2.begin(), strs2.end());
     return strs1;
   }
