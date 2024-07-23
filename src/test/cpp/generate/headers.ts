8bc9d08d618c00dc095d2bf1a9cfed5f1a31e028
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-23 23:09:04 +0700

    feat: add `generateCppIncludeHeadersCode` function

--- src/test/cpp/generate/headers.ts
+++ src/test/cpp/generate/headers.ts
@@ -34,3 +34,16 @@ export class CppHeaders {
       .join("\n");
   }
 }
+
+/**
+ * Generates C++ code for including headers.
+ *
+ * @param headers - Set of headers to include.
+ * @returns The generated C++ code.
+ */
+export function generateCppIncludeHeadersCode(headers: Set<string>): string {
+  return [...headers]
+    .sort()
+    .map((header) => `#include <${header}>`)
+    .join("\n");
+}
