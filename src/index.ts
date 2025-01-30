4a48e9e88796bdc487ee3a0a91eb564a547d308f
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-01-30 22:30:08 +0700

    feat: add entry point function for testing solutions (#559)
    * feat: add `testSolutions` function
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * test: add test for `testSolutions` function
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * chore: remove unused sample files
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export { searchSolutions } from "./search.js";
+import { globSync } from "glob";
+
+export async function testSolutions(patterns: string[]): Promise<string[]> {
+  // TODO: It currently only returns the list of solution files.
+  return patterns
+    .map((pattern) => globSync(pattern))
+    .flat()
+    .sort();
+}
