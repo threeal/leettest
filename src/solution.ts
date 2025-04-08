2a52d2c92351508b2fcd16575aace2783e160fca
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-04-08 21:03:49 +0700

    feat: modify `testSolutions` to search solutions by reading dir (#650)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/solution.ts
+++ src/solution.ts
@@ -1,13 +1,18 @@
-import fsPromises from "node:fs/promises";
+import { readdir } from "node:fs/promises";
 import path from "node:path";
 
 export async function testSolutions(dir: string): Promise<string[]> {
-  const files: string[] = [];
-  const pattern = path.join(dir, "**", "solution.cpp");
-  for await (const file of fsPromises.glob(pattern)) {
-    files.push(file);
+  const childSolutions: Promise<string[]>[] = [];
+  const solutions: string[] = [];
+
+  for (const file of await readdir(dir, { withFileTypes: true })) {
+    if (file.isDirectory()) {
+      childSolutions.push(testSolutions(path.join(dir, file.name)));
+    } else if (file.isFile() && file.name === "solution.cpp") {
+      solutions.push(path.join(dir, file.name));
+    }
   }
 
   // TODO: It currently only returns the list of solution files.
-  return files;
+  return [...(await Promise.all(childSolutions)).flat(), ...solutions];
 }
