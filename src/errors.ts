7735161c2757b86607a9a30fe453896f3dff4a93
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-05-07 22:13:01 +0700

    feat: remove properties in errors (#687)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/errors.ts
+++ src/errors.ts
@@ -1,21 +1,13 @@
 export class CompileError extends AggregateError {
-  file: string;
-
   constructor(error: unknown[], file: string) {
     super(error, `Failed to compile: ${file}`);
 
     this.name = this.constructor.name;
-    this.file = file;
-
     Error.captureStackTrace?.(this, this.constructor);
   }
 }
 
 export class ProcessError extends Error {
-  args: readonly string[];
-  code: number | null;
-  output: string;
-
   constructor(args: readonly string[], code: number | null, output: string) {
     let message = "Process failed";
     if (code !== null) message += ` (${code})`;
@@ -27,23 +19,15 @@ export class ProcessError extends Error {
     super(message);
 
     this.name = this.constructor.name;
-    this.args = args;
-    this.code = code;
-    this.output = output;
-
     Error.captureStackTrace?.(this, this.constructor);
   }
 }
 
 export class RunError extends AggregateError {
-  file: string;
-
   constructor(error: unknown[], file: string) {
     super(error, `Failed to run: ${file}`);
 
     this.name = this.constructor.name;
-    this.file = file;
-
     Error.captureStackTrace?.(this, this.constructor);
   }
 }
